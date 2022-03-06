b0f0f0c469f403a6ccf336fef4f865181f472722
Ahmed Fasih <wuzzyview@gmail.com>
2022-03-05 21:03:23 -0800

    select and store answers and confidences

--- src/App.ts
+++ src/App.ts
@@ -2,14 +2,69 @@ import React, {createElement as ce, useEffect, useState} from 'react';
 
 import {get, Question, QuestionBlock} from './questions';
 
+const SCORE: {[k: string]: {result: boolean|undefined, confidence: number|undefined}} = {};
+const CONFIDENCES = [55, 65, 75, 85, 95];
+
 interface QProps {
   question: Question;
 }
 function Q({question}: QProps) {
-  let q = '';
-  if (question.question) { q = question.question + ' '; }
-  q += question.options.join(' or ') + '?';
-  return ce('li', {}, q);
+  const [choice, setChoice] = useState<number|undefined>(undefined);
+  const [conf, setConf] = useState<number|undefined>(undefined);
+
+  // `unique` is a "hash" of the question, so even if somehow we rerender, it'll be the same
+  const unique = (question.question || '') + question.options.join('');
+  // enroll it in the scoreboard immediately
+  if (!(unique in SCORE)) { SCORE[unique] = {result: undefined, confidence: undefined}; }
+
+  function makeAnswers(content: string, num: number) {
+    const radioGroup = unique + 'question';
+    // <input type="radio" id="dewey" name="drone" value="dewey">
+    // <label for="dewey">Dewey</label>
+    const id = radioGroup + num;
+    return [
+      ce('input', {
+        type: 'radio',
+        id,
+        name: radioGroup,
+        checked: choice === num,
+        onChange: e => {
+          if (e.target.value) {
+            setChoice(num);
+            SCORE[unique].result = num === question.answer;
+            console.log(SCORE[unique])
+          }
+        }
+      }),
+      ce('label', {htmlFor: id}, content)
+    ];
+  };
+  const pairs = question.options.flatMap((option, num) => makeAnswers(option, num));
+
+  function makeConfidences(content: string, num: number) {
+    const radioGroup = unique + 'conf';
+    const id = radioGroup + num;
+    const thisConf = CONFIDENCES[num];
+    return [
+      ce('input', {
+        type: 'radio',
+        id,
+        name: radioGroup,
+        checked: conf === thisConf,
+        onChange: e => {
+          if (e.target.value) {
+            setConf(thisConf);
+            SCORE[unique].confidence = thisConf;
+            console.log(SCORE[unique])
+          }
+        }
+      }),
+      ce('label', {htmlFor: id}, content)
+    ];
+  };
+  const confidences = CONFIDENCES.flatMap((option, num) => makeConfidences(`${option}%`, num))
+
+  return ce('li', {}, (question.question || '') + ' ', ...pairs, ' — confidence: ', ...confidences);
 }
 
 interface BlockProps {
@@ -21,7 +76,7 @@ function Block({block}: BlockProps) {
 
 function App() {
   const [questionBlocks, setQuestionBlocks] = useState<QuestionBlock[]|undefined>(undefined);
-  useEffect(() => { get().then(x => setQuestionBlocks(x)); }, []);
+  useEffect(() => { get().then(list => { setQuestionBlocks(list); }); }, []);
   if (questionBlocks) { return ce('div', null, ...questionBlocks.map(block => ce(Block, {block}))); }
   return ce('div', null, 'Waiting for data!');
 }
